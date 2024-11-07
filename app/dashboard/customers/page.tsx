e277ad552715fd3b619f441e8438cbf353d9847e
gitzein <zeinfm123@gmai.com>
2024-11-07 10:09:40 +0700

    Customer page added

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,32 @@
+import CustomersTable from "@/app/ui/customers/table";
 import { Metadata } from "next";
+import { Suspense } from "react";
+import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
+import Search from "@/app/ui/search";
+import { lusitana } from "@/app/ui/fonts";
 
 export const metadata: Metadata = {
   title: "Customers",
 };
 
-export default function Page() {
-  return <p>Customers Page</p>;
+export default async function Page(props: {
+  searchParams?: Promise<{
+    query?: string;
+  }>;
+}) {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || "";
+
+  return (
+    <div className="w-full">
+      <h1 className={`${lusitana.className} mb-8 text-xl md:text-2xl`}>
+        Customers
+      </h1>
+
+      <Search placeholder="Search customers..." />
+      <Suspense fallback={<InvoicesTableSkeleton />}>
+        <CustomersTable query={query} />
+      </Suspense>
+    </div>
+  );
 }
